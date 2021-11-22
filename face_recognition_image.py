9957f4a98b7d614dcb7379047018d202a715780a
raddanfea <radidani55@gmail.com>
2021-11-22 15:31:11 +0100

    initial commit

---
+++ face_recognition_image.py
@@ -0,0 +1,75 @@
+import face_recognition
+import imutils
+import pickle
+import time
+import cv2
+import os
+
+# load the harcaascade in the cascade classifier
+from imutils import paths
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# load the known faces and embeddings saved in last file
+data = pickle.loads(open('face_enc', "rb").read())
+# Find path to the image you want to detect face and pass it here
+
+images = []
+
+testImagePaths = list(paths.list_images('Test_Images'))
+for t_image in testImagePaths:
+    imbuffer = cv2.imread(t_image)
+    imbuffer = imutils.resize(imbuffer, width=800)
+    images.append(imbuffer)
+
+
+for each in images:
+    image = each
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # convert image to Greyscale for haarcascade
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # the facial embeddings for face in input
+    encodings = face_recognition.face_encodings(rgb)
+    names = []
+    # loop over the facial embeddings incase
+    # we have multiple embeddings for multiple fcaes
+
+    for encoding in encodings:
+        # Compare encodings with encodings in data["encodings"]
+        # Matches contain array with boolean values and True for the embeddings it matches closely
+        # and False for rest
+        matches = face_recognition.compare_faces(data["encodings"],
+                                                       encoding)
+        # set name =inknown if no encoding matches
+        name = "Unknown"
+        # check to see if we have found a match
+        if True in matches:
+            # Find positions at which we get True and store them
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+            # loop over the matched indexes and maintain a count for
+            # each recognized face face
+            for i in matchedIdxs:
+                # Check the names at respective indexes we stored in matchedIdxs
+                name = data["names"][i]
+                # increase count for the name we got
+                counts[name] = counts.get(name, 0) + 1
+                # set name which has highest count
+                name = max(counts, key=counts.get)
+
+            # update the list of names
+            names.append(name)
+            # loop over the recognized faces
+            for ((x, y, w, h), name) in zip(faces, names):
+                # rescale the face coordinates
+                # draw the predicted face name on the image
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(image, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.75, (250, 10, 0), 2)
+        cv2.imshow("Frame", image)
+        cv2.waitKey(0)
